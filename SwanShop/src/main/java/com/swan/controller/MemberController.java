d624ebb4f0fe9066827f92b27f768f8c00712041
flsek <73771163+flsek@users.noreply.github.com>
2022-09-08 16:58:13 +0900

    Update MemberController.java

--- SwanShop/src/main/java/com/swan/controller/MemberController.java
+++ SwanShop/src/main/java/com/swan/controller/MemberController.java
@@ -272,7 +272,7 @@ public class MemberController {
 		logger.info("인증번호 " + checkNum);
 
 		/* 이메일 보내기 */
-		String setFrom = "muchpretty99@naver.com";
+		String setFrom = "본인 이메일";
 		String toMail = email;
 		String title = "회원가입 인증 이메일 입니다.";
 		String content = "SWAN을 방문해주셔서 감사합니다." + "<br><br>" + "인증 번호는 " + checkNum + "입니다." + "<br>"
